81dfaf7ebe26095d3312be6429df150ef164d33c
xavierluiguy <62525734+xavierluiguy@users.noreply.github.com>
2025-07-02 17:35:39 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://xbqzoiqyhobqmakuiwhv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+export const supabase = supabase.createClient(supabaseUrl, supabaseKey);
